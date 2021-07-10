b6ff960af5c89483e417816c8f22b397619453b3
Brittany Doss <brinidoss@gmail.com>
2021-07-10 14:45:55 -0400

    about to get messy refactoring addesigner with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Header from './header/Header';
 import AdDesigner from './adDesigner/AdDesigner';
 import Votes from './votes/Votes';
+import Ad from './ad/Ad';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header user="Sora"/>
+      <Ad flavor="Sea Salt" fontSize={20} darkTheme={false}/>
+      <Ad flavor="Paopu" fontSize={20} darkTheme={true}/>
+      <Ad flavor="Royalberry" fontSize={20} darkTheme={false}/>
       <main>
         <AdDesigner/>
         <Votes/>
--- src/ad/Ad.tsx
+++ src/ad/Ad.tsx
@@ -1,2 +1,28 @@
-import '../src/adDesigner/AdDesigner.css'
+import '../adDesigner/AdDesigner.css'
 
+interface Props {
+    flavor: string;
+    fontSize: number;
+    darkTheme: boolean;
+}
+
+
+
+function Ad({flavor, fontSize, darkTheme}: Props) {
+let addClass = "";
+
+if (darkTheme === false) {
+    addClass = 'light';
+} else {
+    addClass = 'dark';
+}
+
+    return (
+        <div className={`ad-container ${addClass}`}>
+            <p>Vote For</p>
+            <h4 style= {{fontSize: `${fontSize}px`}}>{flavor}</h4>
+        </div>
+    );
+}
+
+export default Ad;
